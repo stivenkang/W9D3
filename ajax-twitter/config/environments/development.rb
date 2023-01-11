d26058cd644220d2fd5352976c3253d45178a859
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-01-11 15:13:27 -0500

    fixed some backend stuff

--- ajax-twitter/config/environments/development.rb
+++ ajax-twitter/config/environments/development.rb
@@ -60,6 +60,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  # require "byebug/core"
-  # Byebug.start_server("localhost", 3001)
+  require "byebug/core"
+  Byebug.start_server("localhost", 3001)
 end
